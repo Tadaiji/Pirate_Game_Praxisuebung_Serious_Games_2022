8d22b7f5d72795128e236211f1d4fbc327066bf7
Tadaiji <40037269+Tadaiji@users.noreply.github.com>
2022-06-24 14:40:19 +0200

    Aktueller Stand

--- Assets/PlayerControllerScript.cs
+++ Assets/PlayerControllerScript.cs
@@ -4,97 +4,174 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerControllerScript : MonoBehaviour
 {
-    
-    
-    public PolygonCollider2D Pcollider;
+
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+
+
+    bool doubleJump = true;
     public BoxCollider2D Bcollider;
     public float speed;
-    public Vector3 movement;
     public float jumpHight;
-    //gravity
-    public float gravity; //-9.81f;
-    
+    bool hit = false;
+    bool hold = false;
+    float hitStart = 0;
+    public int Score = 0;
+    private bool halt = false;
     [SerializeField] public Rigidbody2D body;
     
     [SerializeField] private LayerMask groundLayerMask;
 
-    public float time = 0;
-    private float live = 3;
+    public float live = 3;
+    [SerializeField]
+    private GameObject[] lives;
 
-    private bool isGround()
+    [SerializeField]
+    public Text ScoreUi;
+
+    public GameObject GameManager;
+    Vector2 movement;
+
+    
+    void FixedUpdate()
     {
         
-        float extraHeightText = 1f;
-        RaycastHit2D groundHit = Physics2D.BoxCast(Bcollider.bounds.center , Bcollider.bounds.size, 0f,Vector2.down,  .1f,  groundLayerMask);
-        
-        Color rayColor;
-
-        if (groundHit.collider != null)
+        if (!halt)
         {
-            rayColor = Color.green;
+            Score += 1;
+            ScoreUi.text = "Score: " + Score.ToString();
         }
-        else
+
+        if (hit)
         {
-            rayColor = Color.red;
+            Debug.Log("hitStart: " + hitStart);
+            hitStart += Time.deltaTime;
         }
-        
-        Debug.DrawRay(Bcollider.bounds.center + new Vector3(Bcollider.bounds.extents.x, 0, 0), Vector2.down * (Bcollider.bounds.extents.y + extraHeightText), rayColor);
-        Debug.DrawRay(Bcollider.bounds.center - new Vector3(Bcollider.bounds.extents.x, 0, 0), Vector2.down * (Bcollider.bounds.extents.y + extraHeightText), rayColor);
-        Debug.DrawRay(Bcollider.bounds.center - new Vector3(Bcollider.bounds.extents.x, Bcollider.bounds.extents.y, 0), Vector2.right * Bcollider.bounds.extents.x, rayColor);
-        
-        //Debug.Log(groundHit.collider);
-        //Debug.Log(groundHit.distance);
-        
-        return (groundHit.collider != null && groundHit.distance >= 0.01f);
-        
 
-        return true;
-    }
+        if (hitStart >= 1.5 && hit)
+        {
+            hit = false;
+        }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //movement = new Vector3(0,0,0);
-        time = Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hight = Mathf.Sqrt(jumpHight * gravity * Time.deltaTime);
-        body.velocity = Vector2.right * speed;
 
-        if (isGround() && Input.anyKey)
+        if (!halt)
         {
-            Debug.Log("Jump");
-            body.velocity += (Vector2.up * jumpHight * speed) ;
-            //body.velocity += (Vector2.right * speed);
+            body.velocity = new Vector2(speed, body.velocity.y);
+        }
+
+        if (Input.anyKeyDown && !hold)
+        {
+            //Debug.Log("Jump");
+            body.velocity = new Vector2(body.velocity.x, jumpHight);
+        }
+        else
+        {
+            if (doubleJump && Input.anyKey && !hold)
+            {
+                //Debug.Log("Double Juimp");
+                body.velocity = new Vector2(body.velocity.x, jumpHight);
+                doubleJump = false;
+
+            }
 
         }
-        //movement *= Time.deltaTime;
-        //transform.Translate(movement);
+
+        if (body.velocity.y < 0)
+        {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (body.velocity.y > 0 && !Input.anyKey)
+        {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
 
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        //6 is Enemy layer
-        if(other.gameObject.layer == 6)
-        {
-            Debug.Log("Hit");
-            if(live - 1 >= 0)
-            {
-                live -= 1;
-            }
 
-            if (live == 0)
-            {
-                SceneManager.LoadScene("EndScreen");
-            }
-            
+        if(other.gameObject.tag == "Enemy" && !hit)
+        {
+            HitPlayer();
         }
+    }
+
+    public void HitPlayer()
+    {
+        Debug.Log("Hit");
+        if (live - 1 >= 0)
+        {
+            live -= 1;
+            lives[(int)live].SetActive(false);
+            hit = true;
+
+        }
+
+        if (live == 0)
+        {
+            SceneManager.LoadScene("EndScreen");
+        }
+    }
+
+    //Muss Trigger verwenden, damit man durch den Coin durch springen kann
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("TriggerEnter");
+        if (other.gameObject.tag == "Collectable")
+        {
+            other.GetComponent<Collectable>().DeleteObject();
+            //myObject.GetComponent<MyScript>().MyFunction();
+        }
+        if (other.gameObject.tag == "Interact")
+        {
+            other.GetComponent<InteractScript>().activateSlider();
+        }
+    }
+
+    void OnDisable()
+    {
+        //PlayerPrefs.SetInt("score", playerScore);
+        GameManager.GetComponent<GameManagerScript>().setScore(Score);
+    }
+    
+    public void haltPlayer()
+    {
+        halt = true;
+    }
+
+    public void unhaltPlayer()
+    {
+        halt = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+
+        if (Input.anyKey && other.gameObject.tag == "Interact")
+        {
+            hold = true;
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (hold)
+        {
+            hold = false;
+        }
+
     }
 }
